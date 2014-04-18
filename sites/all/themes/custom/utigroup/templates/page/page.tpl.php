80a1b669122279bf0917919073088476df50a6a2
Abid ANIS <anab.tn@gmail.com>
2014-04-18 17:43:35 +0100

    update home

--- sites/all/themes/custom/utigroup/templates/page/page.tpl.php
+++ sites/all/themes/custom/utigroup/templates/page/page.tpl.php
@@ -4,44 +4,51 @@
  * and open the template in the editor.
  */
 ?>
-<?php include('.' . base_path() . path_to_theme() . '/templates/includes/header.php') ?>	
-<?php print $breadcrumb; ?>
-<!--main -->
-<div class="main">
-	<div class="main-wrapper">
-			<section class="slideshow">
-				<div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 784px; height: 346px;">
-					<!-- Slides Container -->
-					<div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 784px; height: 346px;">
-					<div><img u="image" src="<?php echo base_path().drupal_get_path('theme', 'utigroup'); ?>/assets/img/slideshow/slide1.jpg" /></div>
-					<div><img u="image" src="<?php echo base_path().drupal_get_path('theme', 'utigroup'); ?>/assets/img/slideshow/slide2.jpg" /></div>
-					<div><img u="image" src="<?php echo base_path().drupal_get_path('theme', 'utigroup'); ?>/assets/img/slideshow/slide3.jpg" /></div>
-					<div><img u="image" src="<?php echo base_path().drupal_get_path('theme', 'utigroup'); ?>/assets/img/slideshow/slide4.jpg" /></div>
-					</div>		
-			</section>
-			<section class="fil-actu">texte texte texte texte texte</section>
-			<section class="nos-client"><a href="#"><img src="<?php echo base_path().drupal_get_path('theme', 'utigroup'); ?>/assets/img/logo-mutuelle.jpg" /><a/></section>
-			<section class="dernieres-offres">
-				<h2 class="titre">Nos dernières offres d'emploi</h2>
-				<ul>
-					<li><a href="#">> Analyste Programmeur<br>Grand Système Assurance</a></li>
-					<li><a href="#">> Analyste Développeur <br>Oracle</a></li>
-				</ul>
-			</section>
-			<section class="information">
-				<a href="#" class="block groupe"><span class="fleche"></span>
-					<p class="texte">Toute la puissance de la nouvelle intelligence informatique</p>
-				</a>
-				<a href="#" class="block temoignages"><span class="fleche"></span>
-					<p class="texte">Faites connaissances avec notreentreprise au travers de témoignages de nos collaborateurs</p>
-				</a>
-				<a href="#" class="block rejoigneznous"><span class="fleche"></span>
-				<p class="texte">Découvrez ici notre processus de recrutement et postulez en ligne</p>
-				</a>
-			</section>
-		</div>
-	</div>
 
-	<?php include('.' . base_path() . drupal_get_path('theme', 'utigroup') . '/templates/includes/footer.php') ?>
-</div>
-<!--/page-->
+<div class="page cf">
+
+    <?php if (arg(0) == 'search' && arg(1) == 'site'): ?>         
+
+        <div class="sidebar sidebar-facette">
+            <?php print render($page['sidebar_first']); ?> 
+        </div><!--/sidebar-->
+        <main id="a_content" role="main" >
+            <?php if ($messages): ?>
+                <div id="messages"><div class="section clearfix">
+                        <?php print $messages; ?>
+                    </div></div> <!-- /.section, /#messages -->
+            <?php endif; ?>
+            <?php if ($tabs && $is_admin): ?>
+                <div class="tabs">
+                    <?php print render($tabs); ?>
+                </div>
+            <?php endif; ?>  
+
+
+
+            <?php print render($page['content']); ?>       
+
+        </main>
+
+    <?php else: ?>
+
+        <main id="a_content" role="main" class="box-full">
+            <?php if ($messages): ?>
+                <div id="messages"><div class="section clearfix">
+                        <?php print $messages; ?>
+                    </div></div> <!-- /.section, /#messages -->
+            <?php endif; ?>
+            <?php if ($tabs && $is_admin): ?>
+                <div class="tabs">
+                    <?php print render($tabs); ?>
+                </div>
+            <?php endif; ?>  
+
+
+
+            <?php print render($page['content']); ?>       
+
+        </main>
+    <?php endif; ?>
+
+</div><!--/page-->
