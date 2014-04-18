ab7ace95b2c2ec0670006620162505700e61d7a8
Abid ANIS <anab.tn@gmail.com>
2014-04-18 12:17:51 +0100

    update assets

--- sites/all/themes/custom/utigroup/assets/js/script.js
+++ sites/all/themes/custom/utigroup/assets/js/script.js
@@ -1,4 +1,18 @@
 jQuery(document).ready(function ($) {
-        var options = { $AutoPlay: true };
-        var jssor_slider1 = new $JssorSlider$('slider1_container', options);
-    });
+	var options = {
+		$AutoPlay: true,
+		$DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+
+	   $BulletNavigatorOptions: {                                //[Optional] Options to specify and enable navigator or not
+			$Class: $JssorBulletNavigator$,                       //[Required] Class to create navigator instance
+			$ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+			$AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+			$Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+			$Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+			$SpacingX: 10,                                  //[Optional] Horizontal space between each item in pixel, default value is 0
+			$SpacingY: 10,                                  //[Optional] Vertical space between each item in pixel, default value is 0
+			$Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+		}
+	};
+	var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+});
