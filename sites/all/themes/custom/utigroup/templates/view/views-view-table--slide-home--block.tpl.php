bb347ba9f8889b4a9442376660eafd73a068a46e
Abid ANIS <anab.tn@gmail.com>
2014-04-20 18:38:11 +0100

    page home slide

--- sites/all/themes/custom/utigroup/templates/view/views-view-table--slide-home--block.tpl.php
+++ sites/all/themes/custom/utigroup/templates/view/views-view-table--slide-home--block.tpl.php
@@ -1,14 +1,24 @@
-<div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 784px; height: 346px;">
+<div u="slides" class="home-slider" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 784px; height: 346px;">
     <?php        
 	    foreach ($rows as $row):	    
     ?>
-			<div>
+			<div class="item-slid <?php echo $row['field_orientation_du_texte']; ?>">
 			  <?php
 			   if(!empty($row['field_url_home'])){
 			  ?>
 				<a target="_blank" href="<?php echo $row['field_url_home'];?>">
 			<?php } ?>	
-					<?php echo $row['field_image_home']; ?>
+					<div class="wrap-img slid-img">
+						<?php echo $row['field_image_home']; ?>
+					</div>
+					<div class="wrap-content">
+						<div class="title slid-title">
+							<?php echo $row['title']; ?>
+						</div>
+						<div class="description slid-description">
+							<?php echo $row['body']; ?>
+						</div>
+					</div>
 			 <?php
 			   if(!empty($row['field_url_home'])){
 			  ?>		
